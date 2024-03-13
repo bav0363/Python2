65c283b931ab05a22861721f0b7f9f8c5e0bc9ab
bav0363 <129315926+bav0363@users.noreply.github.com>
2024-03-13 09:26:56 +0300

    Update Homework_26.py

--- Seminar_5/Homework_26.py
+++ Seminar_5/Homework_26.py
@@ -1,13 +1,27 @@
 # Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и
 # возводит число А в целую степень B с помощью рекурсии.
 
-def exponentiate_1(a):
-    if a < 1:
+def exponentiate_1(a, b):
+    if b < 1:
         return 1
-    return a * exponentiate_1(a - 1)
+    return a * exponentiate_1(a, b - 1)
 
 
 a = int(input("Введите число: "))
-# b = int(input("Введите степень числа: "))
-# print(f"Число {a} в степени {b} равно {exponentiate_1(a, b)}")
-print(exponentiate_1(a))
+b = int(input("Введите степень числа: "))
+print(f"Число {a} в степени {b} равно {exponentiate_1(a, b)}")
+
+# Атотест. Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и
+# возводит число А в целую степень B с помощью рекурсии.
+
+def f(a, b):
+    if b < 1:
+        return 1
+    return a * f(a, b - 1)
+
+# Решение из автотеста
+
+def f(a, b):
+  if b == 0:
+    return 1
+  return f(a, b - 1) * a
