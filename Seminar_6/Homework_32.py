a680166266b3bcf3bf8ff8e66ef0fb738239bc4a
bav0363 <129315926+bav0363@users.noreply.github.com>
2024-03-13 09:46:13 +0300

    Update Homework_32.py

--- Seminar_6/Homework_32.py
+++ Seminar_6/Homework_32.py
@@ -17,3 +17,19 @@ print(my_list := [value for value in lst if value >= min_limit and value <= max_
 #     if my_list[i] >= min_limit and my_list[i] <= max_limit:
 #         my_list_2.append(i)
 # print(my_list_2)
+
+# для автотеста 
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = 0
+max_number = 10
+
+for i in range(len(list_1)):
+    if list_1[i] >= min_number and list_1[i] <= max_number:
+         print(i)
+
+# решение из автотеста
+
+for i in range(len(list_1)):
+  if min_number <= list_1[i] <= max_number:
+    print(i)
