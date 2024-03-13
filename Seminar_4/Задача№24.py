0f7b12aadec06cf6a9ef6c2fd4b2365fe0c999ba
bav0363 <129315926+bav0363@users.noreply.github.com>
2024-03-13 09:09:28 +0300

    Update Задача№24.py

--- Seminar_4/Задача№24.py
+++ Seminar_4/Задача№24.py
@@ -16,3 +16,27 @@ for i in range(len(list)):
 print(f"Максимум ягод за один заход: {sum}")
 print("Куст № {} со значением {}".format(index, list[index]))
 
+# решение для автотеста
+
+#arr = [5, 8, 6, 4, 9, 2, 7, 3]
+
+sum = 0
+index = 0
+for i in range(len(arr)):
+    if (arr[i - 1] + arr[i] + arr[(i + 1) % len(arr)]) > sum:
+        sum = arr[i - 1] + arr[i] + arr[(i + 1) % len(arr)]
+        index = i
+print(sum)
+
+# предлагаемое автотестом решение
+
+arr_count = list()
+for i in range(len(arr) - 1):
+    arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+
+# Вывод максимальной урожайности, которую может собрать собирающий модуль
+print(max(arr_count))
+
+
+
